b7015365e599c998c63297fd7d661c1816d0b977
Wingbean <taeey.com@gmail.com>
2023-08-13 10:52:26 +0700

    update library

--- HW/house_price_india.R
+++ HW/house_price_india.R
@@ -1,4 +1,9 @@
-# Load data0
+#load library
+library(tidyverse)
+library(caret)
+library(mlbench)
 library(readxl)
+
+# Load data0
 df1<-read_excel("hpi.xlsx", sheet = 1)
 df2<-read_excel("hpi.xlsx", sheet = 2)
