153b9f98c14fbccdad9f2f0d1c3ff4d224cd1679
Wingbean <taeey.com@gmail.com>
2023-08-22 15:50:27 +0700

    u ncd

--- ml final class.R
+++ ml final class.R
@@ -6,3 +6,34 @@ library(caret)
 library(tidyverse)
 library(mlbench)
 
+data("PimaIndiansDiabetes")
+df <- PimaIndiansDiabetes
+
+## 1. split data
+set.seed(42)
+n <- nrow(df)
+id <- sample(1:n, size=0.8*n)
+train_data <- df[id, ]
+test_data <- df[-id, ]
+
+# Train model
+my_grid<- expand.grid(alpha = 0:1,
+                      lambda = seq(0.0005, 0.05, length=20))
+
+ctrl <- trainControl(method = "cv",
+                     number = 5,
+                     verboseIter = TRUE)
+set.seed(42)
+glmnet_model <- train(diabetes ~ .,
+                        data = train_data,
+                        method = "glmnet",
+                        tuneGrid = my_grid,
+                        trControl = ctrl)
+
+# score
+p<- predict(glmnet_model, newdata = test_data)
+
+# evaluate
+confusionMatrix(p, reference = test_data$diabetes,
+                positive =  "pos",
+                mode = "prec_recall")
